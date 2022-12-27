6968af831a914c46521334c4f12e05e7c8426a8b
C4NU <canu1832@gmail.com>
2022-12-28 01:25:16 +0900

    [ADD] Telegram Call Back Function

--- bots/main.py
+++ bots/main.py
@@ -2,21 +2,26 @@ import os
 import sys
 ###################################
 import dotenv # .env 파일 사용
-import telegram_module as bot # 텔레그램 모듈
+import telegram_module as MessageModule # 텔레그램 모듈
 import twitter_module as twitter # 트위터 모듈
 ###################################
 
 def main():
 	dotenv.load_dotenv()
 
-	token = os.getenv('token')
-	chatID = os.getenv('chatID')
+	telegramBotToken = os.getenv('telegramToken')
+	telegramChatID = os.getenv('telegramChatID')
 
-
-
-	telegram_bot = bot.TelegramBot(token, chatID)
-
+	twitterAPIToken = os.getenv('twitterAPItoken')
+	
+	# initialize Telegram Bot Module
+	bot = MessageModule.TelegramBot(telegramBotToken, telegramChatID)
+	# initialize bot functions
+	bot.HandlerInitialize()
+	# bot start updates
+	bot.updater.start_polling()
+	bot.updater.idle()
 
 if __name__ == '__main__':
 	main()
-	os.execl(sys.executable, sys.executable, *sys.argv)
+	os.execl(sys.executable, sys.executable, *sys.argv) # 에러 시 
--- bots/telegram_module.py
+++ bots/telegram_module.py
@@ -1,8 +1,7 @@
-from setuptools import Command
 import telegram
 
 from telegram.ext import Updater, dispatcher
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 from telegram.ext import MessageHandler, Filters
 
 class TelegramBot:
@@ -12,31 +11,76 @@ class TelegramBot:
 		self.chatID = _chatID
 		self.updater = Updater(token=_token, use_context=True)
 		self.dispatcher = self.updater.dispatcher
-    # 메시지 전송 함수 (self, 메시지 내용)
-	def SendTelegramMessage(self, _description):
-		self.bot.sendMessage(chat_id=self.chatID, text=_description)
-
 	# 봇 시작 함수
 	def StartBot(self, update, context):
-		self.SendTelegramMessage("봇 작동 시작.")
+		self.bot.sendMessage(chat_id=self.chatID, text="Starting Bot...")
 	# 봇 정지 함수
 	def StopBot(self, update, context):
-		self.SendTelegramMessage("봇 작동 종료.")
+		self.bot.sendMessage(chat_id=self.chatID, text="Quiting Bot...")
+	# 계정 / 해시태그 트래킹 입력 함수
 	def SetTracker(self, update, context):
-		self.SendTelegramMessage("팔로우 할 계정 \ 해시태그를 입력해주세요.")
-		self.SendTelegramMessage("계정은 @를 붙이시고, 해시태그는 #을 붙이시면 됩니다.")
+		# 버튼 생성
+		taskButtons = [[
+			telegram.InlineKeyboardButton('Account', callback_data = 1),
+			telegram.InlineKeyboardButton('Hash Tag', callback_data = 2)
+		],[
+			telegram.InlineKeyboardButton('Cancel', callback_data= 3)
+		]]
+		# 버튼 callback 입력받을 변수
+		reply = telegram.InlineKeyboardMarkup(taskButtons)
+		# 버튼 입력하라고 메시지 작성
+		context.bot.sendMessage(
+			chat_id=update.effective_chat.id,
+			text="팔로우 할 것을 선택해주세요.",
+			reply_markup = reply)
 
-    # CommandHandler 생성 함수
+	def CallbackButton(self, update, context):
+		query = update.callback_query
+		data = query.data
+
+		context.bot.send_chat_action(
+			chat_id = update.effective_user.id,
+			action = telegram.ChatAction.TYPING
+		)
+
+		if data == 1:
+			self.SetTrackingAccount(query = query, data = data)
+		elif data == 2:
+			self.SetTrackingHashtag(query = query, data = data)
+
+		context.bot.edit_message_text(
+			chat_id=query.message.chat_id,
+			message_id = query.message.message_id,
+			text = '[{}] Task Complete.'.format(data)
+		)
+	
+	def SetTrackingAccount(self, update, context, query, data):
+		context.bot.edit_message_text(
+		chat_id=query.message.chat_id,
+		message_id = query.message.message_id,
+		text = '[{}] Task Complete.'.format(data)
+		)
+	
+	def SetTrackingHashTag(self, update, context, query, data):
+		context.bot.edit_message_text(
+		chat_id=query.message.chat_id,
+		message_id = query.message.message_id,
+		text = '[{}] Task Complete.'.format(data)
+		)
+	
+	
+
+
+
+	# CommandHandler 생성 함수
 	def HandlerInitialize(self):
 		# Handler 정의 (텔레그램 /"명령어" 인삭)
 		self.startHandler = CommandHandler('start', self.StartBot)
 		self.stopHandler = CommandHandler('stop', self.StopBot)
-		self.setTrackerHandler = CommandHandler('set', self.SetTracker)
+		self.SetTrackerHandler = CommandHandler('set', self.SetTracker)
+		self.CallBackButtonHandler = CallbackQueryHandler(self.CallbackButton)
 		# Handler 추가
 		self.dispatcher.add_handler(self.startHandler)
 		self.dispatcher.add_handler(self.stopHandler)
-		self.dispatcher.add_handler(self.setTrackerHandler)
-
-
-if __name__ == '__main__':
-	main()
+		self.dispatcher.add_handler(self.SetTrackerHandler)
+		self.dispatcher.add_handler(self.CallBackButtonHandler)
